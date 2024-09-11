77add2ce216ff1c9f548ff71c53188c523dda095
web-liuyang <984584014@qq.com>
2024-09-12 00:30:23 +0800

    Feat(Graphic): Image

---
+++ lib/graphics/image.ts
@@ -0,0 +1,78 @@
+import type { CopyWithParameter, GraphicOptions } from "./graphic";
+import { Graphic } from "./graphic";
+
+export interface ImageOptions extends GraphicOptions {
+	image: CanvasImageSource;
+	x: number;
+	y: number;
+	w?: number;
+	h?: number;
+}
+
+export class Image extends Graphic<ImageOptions> {
+	public override readonly type = "Image";
+
+	public readonly image: CanvasImageSource;
+
+	public readonly x: number;
+
+	public readonly y: number;
+
+	public readonly w?: number;
+
+	public readonly h?: number;
+
+	constructor(options: ImageOptions) {
+		super(options);
+		this.image = options.image;
+		this.x = options.x;
+		this.y = options.y;
+		this.w = options.w;
+		this.h = options.h;
+	}
+
+	public paint(ctx: CanvasRenderingContext2D): void {
+		this.draw(ctx, () => {
+			const { image, x, y, w, h } = this;
+			const path = new Path2D();
+
+			if (w !== undefined && h !== undefined) {
+				ctx.drawImage(image, x, y, w, h);
+			} else {
+				ctx.drawImage(image, x, y);
+			}
+
+			ctx.stroke(path);
+			ctx.fill(path);
+		});
+	}
+
+	public copyWith(options: CopyWithParameter<ImageOptions>): Image {
+		return new Image({
+			id: this.id,
+			image: options.image ?? this.image,
+			x: options.x ?? this.x,
+			y: options.y ?? this.y,
+			w: options.w ?? this.w,
+			h: options.h ?? this.h,
+			// selected: options.selected ?? this.selected,
+			// editing: options.editing ?? this.editing,
+			style: options.style ?? this.style,
+		});
+	}
+
+	public hit(point: Point): boolean {
+		return false;
+	}
+
+	public override equals(other: Image): boolean {
+		return (
+			super.equals(other) &&
+			this.image === other.image &&
+			this.x === other.x &&
+			this.y === other.y &&
+			this.w === other.w &&
+			this.h === other.h
+		);
+	}
+}
