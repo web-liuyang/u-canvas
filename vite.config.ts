67b524237a0ea91ac8de8a4266e250ddc863c874
web-liuyang <984584014@qq.com>
2025-05-22 18:01:43 +0800

    Chore: Folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
-import { defineConfig, PluginOption } from "vite";
+import { defineConfig } from "vite";
 // @ts-expect-error uni vite version mismatching
 import uni from "@dcloudio/vite-plugin-uni";
 
-const uCanvasPlugin = (): PluginOption => {
-	return {
-		name: "uCanvasPlugin",
-		enforce: "pre",
-		transform(code, id, options) {
-			const newCode = code
-				.replace(/from "u-canvas"/g, `from "@/uni_modules/u-canvas/index"`)
-				.replace(/from "u-pointer"/g, `from "@/uni_modules/u-pointer/index"`);
-
-			return newCode;
-		},
-	};
-};
-
 export default defineConfig({
-	plugins: [uni(), uCanvasPlugin()],
+	plugins: [uni()],
 });
